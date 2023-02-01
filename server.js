0e33bf83285e25e0069b364aa7337adb63c54dfb
nikunjsetblue <nikunjh@setblue.com>
2023-02-01 11:35:47 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const app = express()
+const port = 3000
+// const router = require('express').Router();
+const books = require('./data');
+
+app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'ejs');
+app.get("/", (request, response) => {
+    response.send("Hi there");
+});
+
+app.listen(3000, () => {
+    console.log("Listen on the port 3000...");
+});
+
+
+let booksDirectory = books;
+
+app.get('/books', function(req, res) {
+    res.sendFile('disp.html', {root: __dirname })
+});
+
+app.get('/data.json', function(req, res) {
+    res.sendFile('data.json', {root: __dirname })
+});
+
+
+app.get('/books/:id', function (req, res) {
+    const { id } = req.params;
+
+    const book = booksDirectory.find(b => b.isbn === id);
+    if (!book) return res.status(404).send('Book does not exist');
+
+    res.render('bookdetails', {
+        book:book
+    });
+});
+
